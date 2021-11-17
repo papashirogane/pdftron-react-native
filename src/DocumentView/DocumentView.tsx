5b2092a65530179295b8c4241eb0da599b055cf0
papashirogane <papashirogane@gmail.com>
2021-11-17 11:50:39 -0800

    Remove comment to trigger Updating JS files

--- src/DocumentView/DocumentView.tsx
+++ src/DocumentView/DocumentView.tsx
@@ -16,8 +16,6 @@ import * as AnnotOptions from "../AnnotOptions/AnnotOptions";
  * Object containing PropTypes types for {@link DocumentView} class.
  * Also used to generate prop types for TS users.
  * 
- * Edit comment to trigger an update to the JS files on the PR head branch.
- * 
  * To represent functions, please use {@link func}.
  * To represent "one of Config.Buttons values" or "an array of 
  * Config.Buttons values", please use {@link oneOf} or {@link arrayOf}.
